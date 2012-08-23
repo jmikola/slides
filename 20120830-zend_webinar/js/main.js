ef0088f641b072548fe5d36588fe22fe4cacda0d
Jeremy Mikola <jmikola@gmail.com>
2012-08-23 10:55:59 -0400

    Zend webinar presentation

---
+++ 20120830-zend_webinar/js/main.js
@@ -0,0 +1,33 @@
+Reveal.initialize({
+    controls: true,
+    progress: true,
+    history: true,
+    transition: Reveal.getQueryHash().transition || "default",
+    dependencies: [
+        { src: "node_modules/reveal.js/lib/js/highlight.js", async: true, callback: function() { window.hljs.initHighlightingOnLoad(); } },
+        { src: "node_modules/reveal.js/lib/js/classList.js", condition: function() { return !document.body.classList; } },
+        { src: "node_modules/reveal.js/lib/js/showdown.js", condition: function() { return !!document.querySelector("[data-markdown]"); } },
+        { src: "node_modules/reveal.js/lib/js/data-markdown.js", condition: function() { return !!document.querySelector("[data-markdown]"); } }
+    ]
+});
+
+Reveal.addEventListener("relational-modeling-diagram", function() {
+    jsPlumb.selectEndpoints({scope: "relational"}).delete();
+
+    var common = {
+        scope: "relational",
+        connector: "StateMachine",
+        paintStyle: { lineWidth: 3, strokeStyle: "white" },
+        endpoint: "Blank",
+        anchor: "Continuous",
+        overlays: [ ["PlainArrow", { location: 1, width: 20, length: 12 } ]]
+    };
+
+    window.setTimeout(function(){
+        jsPlumb.connect({ source: "relational-articles", target: "relational-authors" }, common);
+        jsPlumb.connect({ source: "relational-comments", target: "relational-articles" }, common);
+        jsPlumb.connect({ source: "relational-comments", target: "relational-authors" }, common);
+        jsPlumb.connect({ source: "relational-articles-to-tags", target: "relational-articles" }, common);
+        jsPlumb.connect({ source: "relational-articles-to-tags", target: "relational-tags" }, common);
+    }, 500);
+}, false);
