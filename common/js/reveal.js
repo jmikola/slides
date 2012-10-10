4e7c2bf0b07619d4cae96907906b12b4f81caecd
Jeremy Mikola <jmikola@gmail.com>
2012-10-10 15:47:06 -0400

    Create vendor directory for highlight.js and reveal.js submodules

--- common/js/reveal.js
+++ common/js/reveal.js
@@ -8,10 +8,10 @@ Reveal.initialize({
 
     // Optional libraries used to extend on reveal.js
     dependencies: [
-        { src: '../common/reveal.js/lib/js/highlight.js', async: true, callback: function() { window.hljs.initHighlightingOnLoad(); } },
-        { src: '../common/reveal.js/lib/js/classList.js', condition: function() { return !document.body.classList; } },
-        { src: '../common/reveal.js/lib/js/showdown.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
-        { src: '../common/reveal.js/lib/js/data-markdown.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
-        { src: '../common/reveal.js/plugin/zoom-js/zoom.js', condition: function() { return !!document.body.classList; } }
+        { src: '../vendor/reveal.js/lib/js/highlight.js', async: true, callback: function() { window.hljs.initHighlightingOnLoad(); } },
+        { src: '../vendor/reveal.js/lib/js/classList.js', condition: function() { return !document.body.classList; } },
+        { src: '../vendor/reveal.js/lib/js/showdown.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
+        { src: '../vendor/reveal.js/lib/js/data-markdown.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
+        { src: '../vendor/reveal.js/plugin/zoom-js/zoom.js', condition: function() { return !!document.body.classList; } }
     ]
 });
