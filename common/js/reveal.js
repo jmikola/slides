17f34dd8f58c11066b9e6eb9edd77a91945b6054
Jeremy Mikola <jmikola@gmail.com>
2013-09-10 15:03:29 -0400

    Upgrade slide Markdown for reveal.js 2.5.0

--- common/js/reveal.js
+++ common/js/reveal.js
@@ -9,7 +9,7 @@ Reveal.initialize({
     // Optional libraries used to extend on reveal.js
     dependencies: [
         { src: '../vendor/reveal.js/lib/js/classList.js', condition: function() { return !document.body.classList; } },
-        { src: '../vendor/reveal.js/plugin/markdown/showdown.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
+        { src: '../vendor/reveal.js/plugin/markdown/marked.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
         { src: '../vendor/reveal.js/plugin/markdown/markdown.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
         { src: '../vendor/reveal.js/plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } },
         { src: '../vendor/reveal.js/plugin/zoom-js/zoom.js', async: true, condition: function() { return !!document.body.classList; } }
