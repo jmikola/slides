5c135210105ab65da86184f208625bc072242795
Jeremy Mikola <jmikola@gmail.com>
2013-10-04 23:09:20 +0200

    Upgrade jQuery and jsPlumb and move to common/

--- mongodb_getting_acquainted/js/main.js
+++ mongodb_getting_acquainted/js/main.js
@@ -1,20 +1,21 @@
-Reveal.addEventListener("relational-diagram", function() {
-    jsPlumb.selectEndpoints({scope: "relational"}).delete();
+Reveal.addEventListener("ready", function() {
+    jsPlumb.reset();
 
-    var common = {
-        scope: "relational",
-        connector: "StateMachine",
-        paintStyle: { lineWidth: 3, strokeStyle: "white" },
-        endpoint: "Blank",
-        anchor: "Continuous",
-        overlays: [ ["PlainArrow", { location: 1, width: 20, length: 12 } ]]
-    };
+    jsPlumb.importDefaults({
+        Connector: "StateMachine",
+        PaintStyle: { lineWidth: 3, strokeStyle: "white" },
+        Endpoint: "Blank",
+        Anchor: "Continuous",
+        Overlays: [ ["PlainArrow", { location: 1, width: 20, length: 12 } ]]
+    });
 
-    window.setTimeout(function(){
-        jsPlumb.connect({ source: "relational-articles", target: "relational-authors" }, common);
-        jsPlumb.connect({ source: "relational-comments", target: "relational-articles" }, common);
-        jsPlumb.connect({ source: "relational-comments", target: "relational-authors" }, common);
-        jsPlumb.connect({ source: "relational-articles-to-tags", target: "relational-articles" }, common);
-        jsPlumb.connect({ source: "relational-articles-to-tags", target: "relational-tags" }, common);
-    }, 500);
-}, false);
+    jsPlumb.connect({ source: "relational-articles", target: "relational-authors" });
+    jsPlumb.connect({ source: "relational-comments", target: "relational-articles" });
+    jsPlumb.connect({ source: "relational-comments", target: "relational-authors" });
+    jsPlumb.connect({ source: "relational-articles-to-tags", target: "relational-articles" });
+    jsPlumb.connect({ source: "relational-articles-to-tags", target: "relational-tags" });
+});
+
+Reveal.addEventListener('slidechanged', function() {
+    jsPlumb.repaintEverything();
+});
