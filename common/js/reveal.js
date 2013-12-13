4d41b6e23dd07063f6403c7991e6ea6c2ba82dfc
Jeremy Mikola <jmikola@gmail.com>
2013-12-13 11:18:08 +0100

    Remove whitespace

--- common/js/reveal.js
+++ common/js/reveal.js
@@ -11,8 +11,8 @@ Reveal.initialize({
     // Optional libraries used to extend on reveal.js
     dependencies: [
         { src: '../vendor/reveal.js/lib/js/classList.js', condition: function() { return !document.body.classList; } },
-        { src: '../vendor/reveal.js/plugin/markdown/marked.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
-        { src: '../vendor/reveal.js/plugin/markdown/markdown.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
+        { src: '../vendor/reveal.js/plugin/markdown/marked.js', condition: function() { return !!document.querySelector('[data-markdown]'); } },
+        { src: '../vendor/reveal.js/plugin/markdown/markdown.js', condition: function() { return !!document.querySelector('[data-markdown]'); } },
         { src: '../vendor/reveal.js/plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } },
         { src: '../vendor/reveal.js/plugin/zoom-js/zoom.js', async: true, condition: function() { return !!document.body.classList; } },
         { src: '../vendor/reveal.js/plugin/notes/notes.js', async: true, condition: function() { return !!document.body.classList; } }
